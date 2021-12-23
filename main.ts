3041b07c7e10faf46ee9df9e664d8ccb09dd6622
Dan Stormont <daniel.stormont@gmail.com>
2021-12-22 17:54:16 -0700

    Fix bug in threshold setting.

--- main.ts
+++ main.ts
@@ -2,9 +2,10 @@ basic.showString("Turn on the lights")
 let threshold_light = input.lightLevel()
 basic.showString("Turn off the lights")
 let threshold_dark = input.lightLevel()
-let light_threshold = (threshold_light - threshold_dark) / 2
+let threshold_difference = (threshold_light - threshold_dark) / 2
+let light_threshold = threshold_dark + threshold_difference
 basic.forever(function () {
-    if (input.lightLevel() < 100) {
+    if (input.lightLevel() < light_threshold) {
         basic.showLeds(`
             . . # . .
             . # # # .

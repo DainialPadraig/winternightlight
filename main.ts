cff44edb76e4f78034ff7cc846da858c30533698
Dan Stormont <daniel.stormont@gmail.com>
2021-12-22 17:41:58 -0700

    Add initialization for light level.

--- main.ts
+++ main.ts
@@ -1,11 +1,16 @@
+basic.showString("Turn on the lights")
+let threshold_light = input.lightLevel()
+basic.showString("Turn off the lights")
+let threshold_dark = input.lightLevel()
+let light_threshold = (threshold_light - threshold_dark) / 2
 basic.forever(function () {
-    if (input.lightLevel() < 10) {
+    if (input.lightLevel() < 100) {
         basic.showLeds(`
+            . . # . .
+            . # # # .
+            . # # # .
             # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
+            . . # . .
             `)
     } else {
         basic.clearScreen()
